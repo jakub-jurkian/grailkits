2a339c9b05975ebc64be53bb5d56c40f96d52902
JakubJurkian <kuba.jur03@gmail.com>
2026-04-22 11:07:14 +0200

    feat: create categories database schema migration & move the structure of the folder 1 level above for correct convention

--- apps/catalog-service/src/db/migrations/20260422085756_create_categories_table.js
+++ apps/catalog-service/src/db/migrations/20260422085756_create_categories_table.js
@@ -1,15 +1,12 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+// Migration: Create categories table
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('categories', (table) => {
+    table.increments('id').primary(); // Auto-incrementing primary key
+    table.string('name').notNullable().unique(); // Category name (e.g., 'Retro')
+    table.timestamps(true, true); // Adds created_at and updated_at
+  });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('categories');
 };
