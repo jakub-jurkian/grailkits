18407dc81d24009025074d8d348dd9c3c170a311
JakubJurkian <kuba.jur03@gmail.com>
2026-04-24 15:19:01 +0200

    chore: initialize order service with sequelize and database connection

---
+++ apps/order-service/src/models/order-item.model.js
@@ -0,0 +1,37 @@
+const { DataTypes, Model } = require('sequelize');
+const { sequelize } = require('../config/database');
+
+class OrderItem extends Model {}
+
+OrderItem.init({
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  // pk will be added autom by relation, but I defined it for the clarity
+  orderId: {
+    type: DataTypes.UUID,
+    allowNull: false
+  },
+  productId: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1
+  },
+  unitPrice: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  }
+}, {
+  sequelize,
+  modelName: 'OrderItem',
+  tableName: 'order_items',
+  timestamps: false
+});
+
+module.exports = OrderItem;
